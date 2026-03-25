7ca8ffa4b94761e36f4a27e48b456e50022af1db
Mario Rivera <marioakamomo@gmail.com>
2026-03-25 12:40:54 -0500

    broke it and braught it back to life. Font loading works on local host now

--- js/bangs.js
+++ js/bangs.js
@@ -1,6 +1,7 @@
 bangs = {
   "!w": "https://en.wikipedia.org/wiki/{search}",
   "!yt": "https://www.youtube.com/results?search_query={search}",
+  "!pin": "https://www.pinterest.com/search/pins/?q={search}",
   "!g": "https://www.google.com/search?q={search}",
   "!gh": "https://github.com/search?q={search}",
   "!r": "https://www.reddit.com/search/?q={search}",
@@ -14,7 +15,6 @@ bangs = {
   "!wolf": "https://www.wolframalpha.com/input?i={search}",
   "!arxiv": "https://arxiv.org/search/?query={search}&searchtype=all",
   "!scholar": "https://scholar.google.com/scholar?q={search}",
-  "!pin": "https://www.pinterest.com/search/pins/?q={search}",
   "!mrg": "https://marginalia-search.com/search?query={search}&profile=corpo",
   "!rch":
     "https://wiki.archlinux.org/index.php?search={search}&title=Special%3ASearch",
